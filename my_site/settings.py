7c07e65ec17d6d0a8a6ccfaafd1b146214096e49
aziz <bnmessaoudaziz@gmail.com>
2021-09-13 15:22:12 +0200

    Add Heroku settings and Set DEBUG based on environment variables.

--- my_site/settings.py
+++ my_site/settings.py
@@ -127,3 +127,13 @@ STATIC_URL = '/static/'
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+# Heroku settings.
+import django_heroku
+django_heroku.settings(locals())
+
+
+if os.environ.get('DEBUG') == 'TRUE':
+    DEBUG = True
+elif os.environ.get('DEBUG') == 'FALSE':
+    DEBUG = False
